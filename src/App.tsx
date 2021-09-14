d3eca66cd679e0ea0aa9f22defcd522b17000e8c
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-09-14 11:22:13 +0900

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,7 @@ import { isLoggedInVar } from "./apollo";
 import { LoggedInRouter } from "./routers/logged-in-router";
 import { LoggedOutRouter } from "./routers/logged-out-router";
 
-// const IS_LOGGED_IN = gql`
-//   query isLoggedIn {
-//     isLoggedIn @client
-//   }
-// `;
-
 function App() {
-  // const {
-  //   data: { isLoggedIn },
-  // } = useQuery(IS_LOGGED_IN);
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
