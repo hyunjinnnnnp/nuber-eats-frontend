788cb11bb89cd52ac3c79079a636a3086b0477a4
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-09-16 23:19:18 +0900

    Me Query: Set JWT token

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,9 +1,33 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 import React from "react";
 import { isLoggedInVar } from "../apollo";
+import { meQuery } from "../__generated__/meQuery";
 
-export const LoggedInRouter = () => (
-  <div>
-    <h1>Logged In</h1>
-    <button onClick={() => isLoggedInVar(false)}>Log Out</button>
-  </div>
-);
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
+
+export const LoggedInRouter = () => {
+  //without token >> error FORBIDDEN RESOURCE >> SET JWT TOKEN HEADERS apollo.ts
+  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-medium text-xl tracking-wide">Loading...</span>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{data.me.role}</h1>
+    </div>
+  );
+};
