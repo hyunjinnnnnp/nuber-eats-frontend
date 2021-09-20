d1a05049c19288031d8881452411eeca2e2aee50
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-09-20 17:44:30 +0900

    e2e test: login

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,7 +1,38 @@
-describe("First test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000")
-      .title()
-      .should("eq", "로그lk인 | Nuber Eats");
+describe("Log in", () => {
+  const user = cy;
+  it("should see login page", () => {
+    user.visit("/").title().should("eq", "로그인 | Nuber Eats");
+  });
+  it("can se email and password validation errors", () => {
+    user.visit("/");
+    user.findByPlaceholderText("이메일").type("imhjin");
+    user.findByPlaceholderText("비밀번호").type("1212");
+    user
+      .findAllByRole("alert")
+      .should(
+        "have.text",
+        "잘못된 이메일 형식입니다패스워드는 10자 이상이어야 합니다"
+      );
+    user.findByPlaceholderText("이메일").clear();
+    user.findByPlaceholderText("비밀번호").clear();
+    user
+      .findAllByRole("alert")
+      .should(
+        "have.text",
+        "이메일 주소를 인식할 수 없습니다패스워드를 인식할 수 없습니다"
+      );
+  });
+  it("can fill out the form", () => {
+    user.visit("/");
+    user.findByPlaceholderText("이메일").type("imhjinnnnn@gmail.com");
+    user.findByPlaceholderText("비밀번호").type("1212121212");
+    user
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none")
+      .click();
+    user.window().its("localStorage.nuber-token").should("be.a", "string");
+  });
+  it("sign up", () => {
+    user.visit("/create-account");
   });
 });
