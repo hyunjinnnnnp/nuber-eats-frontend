3f92e9e893166ddc062286e690f519a4420679d3
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-09-28 15:57:35 +0900

    making order part 2: select options

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -9,9 +9,10 @@ interface IDishProps {
   isCustomer?: boolean;
   options?: restaurantQuery_restaurant_restaurant_menu_options[] | null;
   orderStarted?: boolean;
+  isSelected?: boolean;
   addItemToOrder?: (dishId: number) => void;
   removeFromOrder?: (dishId: number) => void;
-  isSelected?: boolean;
+  addOptionToItem?: (dishId: number, option: any) => void;
 }
 
 export const Dish: React.FC<IDishProps> = ({
@@ -22,9 +23,10 @@ export const Dish: React.FC<IDishProps> = ({
   isCustomer = false,
   orderStarted = false, // OWNER component
   options,
+  isSelected,
   addItemToOrder,
   removeFromOrder,
-  isSelected,
+  addOptionToItem,
 }) => {
   const onClick = () => {
     if (orderStarted) {
@@ -38,25 +40,36 @@ export const Dish: React.FC<IDishProps> = ({
   };
   return (
     <div
-      onClick={
-        // () =>
-        // orderStarted && !isSelected && addItemToOrder ? addItemToOrder(id) : null
-        onClick
-      }
       className={`px-8 py-4 border transition-all ${
         isSelected ? "border-gray-800" : "hover:border-gray-800 cursor:pointer"
       }`}
     >
       <div className="mb-5">
+        <h3 className="font-medium">
+          {name}{" "}
+          {orderStarted && (
+            <button onClick={onClick}>{isSelected ? "Remove" : "Add"}</button>
+          )}
+        </h3>
         <h4 className="text-lg font-medium">{description}</h4>
-        <h3 className="font-medium">{name}</h3>
       </div>
       <span>${price}</span>
       {isCustomer && options && options?.length !== 0 && (
         <div>
           <h5 className="mt-8 mb-3 font-medium">Dish Options:</h5>
           {options?.map((option, index) => (
-            <span className="flex items-center" key={index}>
+            <span
+              onClick={() =>
+                addOptionToItem
+                  ? addOptionToItem(id, {
+                      name: option.name,
+                      extra: option.extra,
+                    })
+                  : null
+              }
+              className="flex items-center border"
+              key={index}
+            >
               <h6 className="mr-2">{option.name}</h6>
               <h6 className="text-sm opacity-75">(${option.extra})</h6>
             </span>
