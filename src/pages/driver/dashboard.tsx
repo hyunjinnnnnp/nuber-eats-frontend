60844f06a66897081aaaceb216e1c1d0888d5b5b
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-10-01 20:25:05 +0900

    Driver Dashboard: google Directions API

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -36,14 +36,15 @@ export const Dashbord = () => {
     if (map && maps) {
       map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
       const geocoder = new google.maps.Geocoder();
-      geocoder.geocode(
-        {
-          location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
-        },
-        (results, status) => {
-          console.log(status, results);
-        }
-      );
+      //GEOCODING : coords -> address
+      //   geocoder.geocode(
+      //     {
+      //       location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+      //     },
+      //     (results, status) => {
+      //       console.log(status, results);
+      //     }
+      //   );
     }
   }, [driverCoords.lat, driverCoords.lng, map, maps]);
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
@@ -52,6 +53,39 @@ export const Dashbord = () => {
     map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
     setMap(map);
     setMaps(maps);
+  };
+  const onGetRouteClick = () => {
+    if (map) {
+      const directionsService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer({
+        polylineOptions: {
+          strokeColor: "#000",
+          strokeOpacity: 0.7,
+          strokeWeight: 3,
+        },
+      });
+      directionsRenderer.setMap(map);
+      directionsService.route(
+        {
+          origin: {
+            location: new google.maps.LatLng(
+              driverCoords.lat,
+              driverCoords.lng
+            ),
+          },
+          destination: {
+            location: new google.maps.LatLng(
+              driverCoords.lat + 0.05,
+              driverCoords.lng + 0.05
+            ),
+          },
+          travelMode: google.maps.TravelMode.DRIVING,
+        },
+        (result) => {
+          directionsRenderer.setDirections(result);
+        }
+      );
+    }
   };
   return (
     <div>
@@ -73,6 +107,7 @@ export const Dashbord = () => {
           <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
         </GoogleMapReact>
       </div>
+      <button onClick={onGetRouteClick}>Get Route</button>
     </div>
   );
 };
